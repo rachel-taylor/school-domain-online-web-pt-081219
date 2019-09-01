bc9013f7979e347bd7719c5067306f4960eb471e
Rachel T <rachmaybe@gmail.com>
2019-09-01 03:25:12 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -10,7 +10,23 @@ class School
     @roster
   end 
   
-  def add_student
-    @add_student
-  end 
+  def add_student(name, grade)
+    @student_name = name
+    @grade = grade
+    if @roster.include?(grade) == false
+      @roster[grade] = []
+    end
+    @roster[grade] << name
+  end
+
+  def grade(number)
+    @roster[number]
+  end
+
+  def sort
+    @roster.each do |grade, name|
+      @roster[grade] = name.sort
+    end
+  end
+
 end 
